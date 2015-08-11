16ee4f17c5267f422084ad2479366dc1e30d38e5
Matthew Beaumont <matt@beaumontwebdev.com>
2015-08-11 19:05:46 -0400

    Video 5.3

--- Code/monstermash/assets/AppAsset.php
+++ Code/monstermash/assets/AppAsset.php
@@ -25,5 +25,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'hiqdev\assets\lightbox2\LightboxAsset'
     ];
 }
